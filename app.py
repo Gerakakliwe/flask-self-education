2a6dfb02174e2b9da77bfc1df8f290997c8490a5
bbondarenko <bogdan.bondarenko99@gmail.com>
2022-05-23 08:17:59 -0700

    add more pages with parameters

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def main_page():
-    return "<p>Main page</p>"
+    return '<p>Main page</p>'
+
+
+@app.route('/user/<int:user_id>')
+def show_user_profile(user_id):
+    return f'User {escape(user_id)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
